2f9de70e8dab6d0b72170d6a8f1c2c8aed064a49
wovvscion 93 <wovvscion.93@tcpl.com>
2025-03-24 16:05:57 +0530

    Removed unwanted file

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d D:\Neeraj_Documents\Code\FactoHR_AutoPunch\main.py
-call venv\Scripts\activate
-python main.py
-echo.
-echo Script execution completed. Press any key to exit...
-pause
